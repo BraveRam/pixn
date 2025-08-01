28dd3b9db91a3f19bcb1227ed1bc994c05c4a4d4
Braveram <lenchomengistu100@gmail.com>
2025-08-01 19:05:23 +0300

    fix: fixed sign out

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import MobileMenu from "./MobileMenu";
+import { redirect } from "next/navigation";
 
 const Navbar = async () => {
   const supabase = await createClient();
@@ -11,19 +12,23 @@ const Navbar = async () => {
 
   const fullName = user?.user_metadata?.full_name ?? "";
 
+  const signOut = async () => {
+    "use server";
+    await supabase.auth.signOut();
+    redirect("/auth/sign-in");
+  };
+
   return (
     <nav className="bg-black/80 backdrop-blur-md text-white flex items-center justify-between px-6 py-4 border-b border-white/10 fixed top-0 left-0 right-0 z-50 shadow-md">
       <Link href="/">
         <h1 className="font-bold text-3xl">Pixn</h1>
       </Link>
-
       <div className="flex items-center gap-4">
         <Link href="/upload">
           <Button className="bg-blue-500 text-white font-extrabold hover:bg-blue-700">
             Upload
           </Button>
         </Link>
-
         <div className="hidden sm:flex items-center gap-4">
           <Link href="/gallery">
             <Button variant="outline">Dashboard</Button>
@@ -32,14 +37,13 @@ const Navbar = async () => {
             <Button variant="outline">Favorites</Button>
           </Link>
           <p className="font-bold">{fullName}</p>
-          <form action="/auth/sign-out" method="post">
+          <form action="signOut">
             <Button variant={"destructive"} type="submit">
               Sign Out
             </Button>
           </form>
         </div>
 
-        {/* Mobile menu toggle */}
         <div className="sm:hidden">
           <MobileMenu fullName={fullName} />
         </div>
