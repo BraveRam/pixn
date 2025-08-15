bb7b6ef9398fced8407738d4a3a193e87d5a88e7
Braveram <lenchomengistu100@gmail.com>
2025-08-15 15:15:59 +0300

    modify: added suspense on the gallery page

---
+++ app/(dashboard)/gallery/Gallery.tsx
@@ -0,0 +1,53 @@
+import { createClient } from "@/utils/supabase/server";
+import ImageList from "./ImageList";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Images, Loader2 } from "lucide-react";
+import { Suspense } from "react";
+
+export default async function Gallery() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const { data, error } = await supabase.storage.from("images").list(user?.id, {
+    limit: 100,
+    offset: 0,
+    sortBy: { column: "created_at", order: "desc" },
+  });
+
+  const signedUrlsRes = await supabase.storage.from("images").createSignedUrls(
+    data?.map((file) => `${user?.id}/${file.name}`) || [],
+    60 * 60 * 24 * 30 // 30 days
+  );
+
+  const images = (signedUrlsRes.data || [])
+    .filter((img) => img.path !== null)
+    .map((img) => ({
+      ...img,
+      path: img.path!,
+      favorite: false,
+    }));
+
+  return images.length === 0 ? (
+    <div className="text-center space-y-4 mt-8">
+      <h2 className="text-2xl font-semibold">
+        You haven’t uploaded any images yet.
+      </h2>
+      <Link href="/upload" prefetch>
+        <Button className="text-base">Upload Images</Button>
+      </Link>
+    </div>
+  ) : (
+    <>
+      <h1 className="text-3xl font-extrabold text-center mb-8">
+        <span className="flex items-center justify-center gap-2">
+          <Images />
+          Gallery
+        </span>
+      </h1>
+      <ImageList images={images} />
+    </>
+  );
+}
--- app/(dashboard)/gallery/page.tsx
+++ app/(dashboard)/gallery/page.tsx
@@ -1,36 +1,9 @@
 // app/dashboard/page.tsx
-import { createClient } from "@/utils/supabase/server";
-import ImageList from "./ImageList";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { Images, Loader2 } from "lucide-react";
 import { Suspense } from "react";
+import { Loader2 } from "lucide-react";
+import Gallery from "./gallery";
 
-export default async function Page() {
-  const supabase = await createClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  const { data, error } = await supabase.storage.from("images").list(user?.id, {
-    limit: 100,
-    offset: 0,
-    sortBy: { column: "created_at", order: "desc" },
-  });
-
-  const signedUrlsRes = await supabase.storage.from("images").createSignedUrls(
-    data?.map((file) => `${user?.id}/${file.name}`) || [],
-    60 * 60 * 24 * 30 // 30 days
-  );
-
-  const images = (signedUrlsRes.data || [])
-    .filter((img) => img.path !== null)
-    .map((img) => ({
-      ...img,
-      path: img.path!,
-      favorite: false,
-    }));
-
+export default function Page() {
   return (
     <div className="flex flex-col items-center gap-6 px-3 py-8 max-w-7xl mx-auto">
       <Suspense
@@ -40,26 +13,7 @@ export default async function Page() {
           </div>
         }
       >
-        {images.length === 0 ? (
-          <div className="text-center space-y-4 mt-8">
-            <h2 className="text-2xl font-semibold">
-              You haven’t uploaded any images yet.
-            </h2>
-            <Link href="/upload" prefetch>
-              <Button className="text-base">Upload Images</Button>
-            </Link>
-          </div>
-        ) : (
-          <>
-            <h1 className="text-3xl font-extrabold text-center mb-8">
-              <span className="flex items-center justify-center gap-2">
-                <Images />
-                Gallery
-              </span>
-            </h1>
-            <ImageList images={images} />
-          </>
-        )}
+        <Gallery />
       </Suspense>
     </div>
   );
