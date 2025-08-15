31aa1e8a64a944c0ecbad77e5757368d3b9826d9
Braveram <lenchomengistu100@gmail.com>
2025-08-15 15:10:42 +0300

    modify: added suspense on the gallery page

--- app/(dashboard)/gallery/page.tsx
+++ app/(dashboard)/gallery/page.tsx
@@ -3,7 +3,8 @@ import { createClient } from "@/utils/supabase/server";
 import ImageList from "./ImageList";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Images } from "lucide-react";
+import { Images, Loader2 } from "lucide-react";
+import { Suspense } from "react";
 
 export default async function Page() {
   const supabase = await createClient();
@@ -32,26 +33,34 @@ export default async function Page() {
 
   return (
     <div className="flex flex-col items-center gap-6 px-3 py-8 max-w-7xl mx-auto">
-      {images.length === 0 ? (
-        <div className="text-center space-y-4 mt-8">
-          <h2 className="text-2xl font-semibold">
-            You haven’t uploaded any images yet.
-          </h2>
-          <Link href="/upload" prefetch>
-            <Button className="text-base">Upload Images</Button>
-          </Link>
-        </div>
-      ) : (
-        <>
-          <h1 className="text-3xl font-extrabold text-center mb-8">
-            <span className="flex items-center justify-center gap-2">
-              <Images />
-              Gallery
-            </span>
-          </h1>
-          <ImageList images={images} />
-        </>
-      )}
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center h-96 w-96">
+            <Loader2 className="animate-spin text-white" />
+          </div>
+        }
+      >
+        {images.length === 0 ? (
+          <div className="text-center space-y-4 mt-8">
+            <h2 className="text-2xl font-semibold">
+              You haven’t uploaded any images yet.
+            </h2>
+            <Link href="/upload" prefetch>
+              <Button className="text-base">Upload Images</Button>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <h1 className="text-3xl font-extrabold text-center mb-8">
+              <span className="flex items-center justify-center gap-2">
+                <Images />
+                Gallery
+              </span>
+            </h1>
+            <ImageList images={images} />
+          </>
+        )}
+      </Suspense>
     </div>
   );
 }
